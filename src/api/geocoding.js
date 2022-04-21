8360b295b0751e5d774d1e191e609ed75f61c5aa
Edison Blum <blum.edison12@gmail.com>
2022-04-21 11:09:31 -0400

    API fixes

--- src/api/geocoding.js
+++ src/api/geocoding.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 const WEATHER_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 export default axios.create({
-	baseURL: `http://api.openweathermap.org/geo/1.0/direct?&limit=1&appid=${WEATHER_KEY}`,
+	baseURL: `https://api.openweathermap.org/geo/1.0/direct?&limit=1&appid=${WEATHER_KEY}`,
 });
